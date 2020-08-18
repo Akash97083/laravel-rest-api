480c77f9c7945f29e95ec01c4e24a982001dff93
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-08-18 21:04:59 +0600

    Project add to git

---
+++ app/Http/Controllers/BuyerController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Buyer;
+use Illuminate\Http\Request;
+
+class BuyerController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Buyer $buyer)
+    {
+        //
+    }
+
+    public function update(Request $request, Buyer $buyer)
+    {
+        //
+    }
+
+    public function destroy(Buyer $buyer)
+    {
+        //
+    }
+}
---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Category;
+use Illuminate\Http\Request;
+
+class CategoryController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Category $Category)
+    {
+        //
+    }
+
+    public function update(Request $request, Category $Category)
+    {
+        //
+    }
+
+    public function destroy(Category $Category)
+    {
+        //
+    }
+}
