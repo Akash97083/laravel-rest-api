3a15a2621119c67d1a4cb912d31746f3ad932576
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-08-19 22:22:43 +0600

    table field define in migration file

--- database/migrations/2020_08_18_142016_create_products_table.php
+++ database/migrations/2020_08_18_142016_create_products_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Model\Product;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,7 +16,15 @@ class CreateProductsTable extends Migration
     {
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->unsignedBigInteger('seller_id');
+            $table->string('name');
+            $table->text('description');
+            $table->unsignedBigInteger('quantity');
+            $table->string('status')->default(Product::UNAVAILABLE_PRODUCT);
+            $table->string('image')->nullable();
             $table->timestamps();
+
+            $table->foreign('seller_id')->references('id')->on('users');
         });
     }
 
