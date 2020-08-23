68e333d83938363cace984a2bd255bbc5d2f9bcd
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-08-24 00:05:11 +0600

    seller and buyer global scope added

--- app/Model/Buyer.php
+++ app/Model/Buyer.php
@@ -2,11 +2,23 @@
 
 namespace App\Model;
 
+use App\Scopes\BuyerScope;
+use Illuminate\Database\Eloquent\Builder;
+
 /**
  * @method static has(string $string)
  */
 class Buyer extends User
 {
+    protected static function booted()
+    {
+        // static::addGlobalScope(new BuyerScope);
+
+        static::addGlobalScope('buyer', function (Builder $builder) {
+            $builder->has('transactions');
+        });
+    }
+
     public function transactions()
     {
         return $this->hasMany(Transaction::class);
--- app/Model/Seller.php
+++ app/Model/Seller.php
@@ -2,11 +2,22 @@
 
 namespace App\Model;
 
+use Illuminate\Database\Eloquent\Builder;
+
 /**
  * @method static has(string $string)
  */
 class Seller extends User
 {
+    protected static function booted()
+    {
+        // static::addGlobalScope(new SellerScope);
+
+        static::addGlobalScope('seller', function (Builder $builder) {
+            $builder->has('products');
+        });
+    }
+
     public function products() {
         return $this->hasMany(Product::class);
     }
