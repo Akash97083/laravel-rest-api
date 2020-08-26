8842c3c67cbada4eceb2d6d7563fe7887d6b8f7f
Abdullah al mamun <abdullah.ssc13@gmail.com>
2020-08-26 20:50:52 +0600

    get category transactions

---
+++ app/Http/Controllers/Category/CategoryTransactionController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Category;
+
+use App\Http\Controllers\ApiController;
+use App\Http\Controllers\Controller;
+use App\Model\Category;
+use Illuminate\Http\Request;
+
+class CategoryTransactionController extends ApiController
+{
+    public function index(Category $category)
+    {
+        $transactions = $category->products()->has('transactions')
+            ->with('transactions')
+            ->get()
+            ->pluck('transactions')
+            ->collapse();
+
+        return $this->successResponse(['transactions' => $transactions], 200);
+    }
+}
